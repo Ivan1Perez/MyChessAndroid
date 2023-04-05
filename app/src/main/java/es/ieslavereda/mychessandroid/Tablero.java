23c9e2de03a9e55e4e3024b0d7f5f98cdabced8e
Ivan Perez <ivan_psh@hotmail.com>
2023-04-05 14:04:38 +0200

    Update

--- app/src/main/java/es/ieslavereda/mychessandroid/Tablero.java
+++ app/src/main/java/es/ieslavereda/mychessandroid/Tablero.java
@@ -101,61 +101,6 @@ public class Tablero extends TableLayout {
     }
 
     /**
-     * Place pieces.
-     *
-     * @param color the color
-     */
-    public void placePieces(Color color){
-        int whites = 8, blacks = 1, whitePawns = 7, blackPawns = 2;
-        char queenPosition = 'D', kingPosition = 'E';
-
-        if(color==Color.BLACK){
-            whitesUp = true;
-            whites = 1;
-            blacks = 8;
-            whitePawns = 2;
-            blackPawns = 7;
-            queenPosition = 'E';
-            kingPosition = 'D';
-        }
-//---------------------------- PARA DEMOSTRACIÓN----------------------------------------------------------------
-//        deletedPieces.add(new CaballoBlanco(getCelda(new Coordenada('A', blacks))));
-//        deletedPieces.add(new AlfilBlanco(getCelda(new Coordenada('A', blacks))));
-//        deletedPieces.add(new ReyBlanco(getCelda(new Coordenada('A', blacks))));
-//        deletedPieces.add(new ReinaBlanca(getCelda(new Coordenada('A', blacks))));
-//        deletedPieces.add(new AlfilBlanco(getCelda(new Coordenada('A', blacks))));
-//        deletedPieces.add(new CaballoBlanco(getCelda(new Coordenada('A', blacks))));
-//        deletedPieces.add(new TorreNegra(getCelda(new Coordenada('A', blacks))));
-//
-//        getRemainigPieces().add(new PeonBlanco(getCelda(new Coordenada(((char)('H')), 2))));
-//        getRemainigPieces().add(new PeonBlanco(getCelda(new Coordenada(((char)('E')), 2))));
-//        getRemainigPieces().add(new PeonNegro(getCelda(new Coordenada(((char)('D')), 7))));
-//---------------------------- PARA DEMOSTRACIÓN---------------------------------------------------------------
-
-
-//        getRemainigPieces().add(new TorreNegra(getCelda(new Coordenada('A', blacks))));
-//        getRemainigPieces().add(new CaballoNegro(getCelda(new Coordenada('B',blacks))));
-//        getRemainigPieces().add(new AlfilNegro(getCelda(new Coordenada('C',blacks))));
-//        getRemainigPieces().add(new ReyNegro(getCelda(new Coordenada(kingPosition,blacks))));
-////        getRemainigPieces().add(new ReinaNegra(getCelda(new Coordenada(queenPosition,blacks))));
-//        getRemainigPieces().add(new AlfilNegro(getCelda(new Coordenada('F',blacks))));
-//        getRemainigPieces().add(new CaballoNegro(getCelda(new Coordenada('D',6))));
-//        getRemainigPieces().add(new TorreNegra(getCelda(new Coordenada('H', blacks))));
-//        for(int i = 0 ; i < 8 ; i++){
-//            getRemainigPieces().add(new PeonNegro(getCelda(new Coordenada(((char)('A'+ i)), blackPawns))));
-//            getRemainigPieces().add(new PeonBlanco(getCelda(new Coordenada(((char)('H'- i)), whitePawns))));
-//        }
-//        getRemainigPieces().add(new TorreBlanca(getCelda(new Coordenada('A',whites))));
-//        getRemainigPieces().add(new CaballoBlanco(getCelda(new Coordenada('B',whites))));
-//        getRemainigPieces().add(new AlfilBlanco(getCelda(new Coordenada('C', whites))));
-//        getRemainigPieces().add(new ReinaBlanca(getCelda(new Coordenada('B',blacks))));
-//        getRemainigPieces().add(new ReyBlanco(getCelda(new Coordenada(kingPosition, whites))));
-//        getRemainigPieces().add(new AlfilBlanco(getCelda(new Coordenada('F', whites))));
-//        getRemainigPieces().add(new CaballoBlanco(getCelda(new Coordenada('G',whites))));
-//        getRemainigPieces().add(new TorreBlanca(getCelda(new Coordenada('H',whites))));
-    }
-
-    /**
      * Highlight king.
      *
      * @param coordenada the coordenada
